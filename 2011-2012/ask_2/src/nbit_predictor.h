7290f976b717846db47d966a7592562c6fa7960b
Gregory Liras <gregliras@gmail.com>
2012-05-20 16:51:48 +0300

    Init

---
+++ 2011-2012/ask_2/src/nbit_predictor.h
@@ -0,0 +1,35 @@
+// nbit_predictor.h
+//
+//
+
+#ifndef N_BIT_PREDICTOR_H
+#define N_BIT_PREDICTOR_H
+
+
+#include <math.h>
+#include <string.h>
+#include "predictor.h"
+
+class nbit_update : public branch_update
+{
+public:
+    unsigned int index;
+};
+
+class nbit_predictor : public branch_predictor
+{
+public:
+#define NBP_TABLE_BITS	15  //number of entries = 2^15
+    nbit_update u;
+    branch_info bi;
+    int counter_limit;
+    int N_COUNTER_LENGTH;
+
+    unsigned char tab[1<<NBP_TABLE_BITS];
+
+    nbit_predictor (int length);
+    branch_update *predict (branch_info & b);
+    void update (branch_update *u, bool taken, unsigned int target);
+};
+
+#endif
